cd788be6ce3c97ff34da9b1f29d337ce07d9a05b
hayato <hayato.drsp@gmail.com>
2021-11-15 21:05:24 +0900

    Finish_Chapter_12_of_the_rails_tutorial

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,62 +1,78 @@
 class User < ApplicationRecord
-    has_secure_password
-    attr_accessor :remember_token, :activation_token
-    before_save   :downcase_email
-    before_create :create_activation_digest
+  has_secure_password
+  attr_accessor :remember_token, :activation_token, :reset_token
 
-    validates :name, presence: true, length: {maximum: 50}
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true,length:{maximum: 255}, format: { with: VALID_EMAIL_REGEX}, uniqueness:{case_sensitive: false}
-    validates :password, presence: true, length:{minimum: 6}, allow_nil: true
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
-    
-    
-    # 渡された文字列のハッシュ値を返す
-    def User.digest(string)
-        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                    BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
-    end
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-    def User.new_token
-        SecureRandom.urlsafe_base64
-    end
+  # 渡された文字列のハッシュ値を返す
+  def self.digest(string)
+    cost = if ActiveModel::SecurePassword.min_cost
+             BCrypt::Engine::MIN_COST
+           else
+             BCrypt::Engine.cost
+           end
+    BCrypt::Password.create(string, cost: cost)
+  end
 
-    def remember
-        self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))
-    end
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
 
-    # トークンがダイジェストと一致したらtrueを返す
-    def authenticated?(attribute, token)
-        digest = send("#{attribute}_digest")
-        return false if digest.nil?
-        BCrypt::Password.new(digest).is_password?(token)
-    end
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
 
-    def forget
-        update_attribute(:remember_digest, nil)
-    end
+  # トークンがダイジェストと一致したらtrueを返す
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
 
-    def activate
-        update_columns(activated: true, activated_at: Time.zone.now)
-    end
+    BCrypt::Password.new(digest).is_password?(token)
+  end
 
-    def send_activation_email
-        UserMailer.account_activation(self).deliver_now
-    end
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
-    private
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
 
-        def downcase_email
-            self.email = email.downcase
-        end
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
 
-        # 有効化トークンとダイジェストを作成および代入する
-        def create_activation_digest
-            self.activation_token  = User.new_token
-            self.activation_digest = User.digest(activation_token)
-        end
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
 
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
 
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
